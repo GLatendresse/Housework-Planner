5893c7c900590a49dadcbfdee7f1479ad4c93278
GLatendresse <gabriel.latendresse@gmail.com>
2015-04-03 18:21:41 -0400

    Test de gui designer

---
+++ src/MainView.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * Created by gabri_000 on 2015-04-03.
+ */
+public class MainView extends JFrame{
+    private JButton button1;
+    private JPanel panel1;
+
+    public MainView(JButton button1, JPanel panel1) {
+        this.button1 = button1;
+        this.panel1 = panel1;
+    }
+}
